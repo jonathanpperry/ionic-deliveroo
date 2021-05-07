712977985bcd62e1d780cd02efdbdd258f01c91e
Jonathan Perry <jonnyintokyo@gmail.com>
2021-05-07 12:52:41 +0900

    Generated directives and added to exports and imported in home/details modules, copied fonts and finished the ionic slides section

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  categories = [];
+  highlights = [];
+  featured = [];
 
-  constructor() {}
+  catSlideOpts = {
+    freeMode: true,
+    slidesPerView: 3.5,
+    slidesOffsetBefore: 11,
+    spaceBetween: 10,
+  };
 
+  highlightSlideOpts = {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+  };
+
+  featuredSlideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json')
+      .subscribe((res: any) => {
+        this.categories = res.categories;
+        this.highlights = res.highlights;
+        this.featured = res.featured;
+      });
+  }
 }
